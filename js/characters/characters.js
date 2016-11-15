6711f55a863af50608f562d1e0aced2da4c9414d
Anna <_djes@ua.fm>
2016-11-15 22:33:36 +0200

    add characters

--- js/characters/characters.js
+++ js/characters/characters.js
@@ -188,7 +188,271 @@ var characters = [
         "years": 25,
         "gender": "female"
 
+    },
+    //!!! Обновить возраст у всех персонажей ниже
+    // Marvel Comics | Wolverine | James Howlett | Regenerative healing factor | 176cm | 166kg | man | mutant | Cameo: The Incredible Hulk #180 (Oct. 1974) | black | Not good enough
+    {
+        "id": 8,
+        "img": "Wolverine.jpg",
+        "name": "Wolverine",
+        "alterEgo": "James Howlett",
+        "height": 176,
+        "weight": 166,
+        "years": 40,
+        "gender": "man"
+    },
+    // DC Comics | Green Lantern | Kyle Rayner | Green Lantern Corps Power Ring | 180cm | 82kg | man | human | Green Lantern (vol. 3) #48 (January 1994) | brown | Not good enough
+    {
+        "id": 9,
+        "img": "Green-Lantern.jpg",
+        "name": "Green Lantern",
+        "alterEgo": "Kyle Rayner",
+        "height": 180,
+        "weight": 82,
+        "years": 35,
+        "gender": "man"
+    },
+    // DC Comics | Deathstroke | Slade Joseph Wilson | Regenerative healing factor | 193cm | 102kg | man | human | Slade Wilson as Deathstroke. Cover to Deathstroke (vol. 2) #8 | white | Bad
+    {
+        "id": 10,
+        "img": "Deathstroke.jpg",
+        "name": "Deathstroke",
+        "alterEgo": "Slade Joseph Wilson",
+        "height": 193,
+        "weight": 102,
+        "years": 45,
+        "gender": "man"
+    },
+    // Marvel Comics | Silver Surfer | Norrin Radd | Endowed with the Power Cosmic | 195cm | 92kg | man | alien | The Silver Surfer #1 (Aug. 1968) | no hair | Strange
+    {
+        "id": 11,
+        "img": "Silver-Surfer.jpg",
+        "name": "Silver Surfer",
+        "alterEgo": "Norrin Radd",
+        "height": 195,
+        "weight": 92,
+        "years": 33,
+        "gender": "man"
+    },
+    // Marvel Comics | The Punisher | Franken-Castle | Punisher | 180cm | 90kg | man | human | Cover to The Punisher #1(1986) | black | Strange
+    {
+        "id": 12,
+        "img": "Punisher.jpg",
+        "name": "The Punisher",
+        "alterEgo": "Franken-Castle",
+        "height": 180,
+        "weight": 90,
+        "years": 33,
+        "gender": "man"
+    },
+    // DC Comics | Doctor Fate | Kent Nelson | Mastery of magic | 187cm | 90kg | man | human | More Fun Comics #55 (May 1940) | blond | Neutral
+    {
+        "id": 13,
+        "img": "Doctor-Fate.jpg",
+        "name": "Doctor Fate",
+        "alterEgo": "Kent Nelson",
+        "height": 187,
+        "weight": 90,
+        "years": 35,
+        "gender": "man"
+    },
+    // Marvel Comics | Thing | Benjamin 'Ben' Grimm | Superhuman strength | 190cm | 200kg | man | mutant | The Fantastic Four #1 (November 1961) | no hair | Good
+    {
+        "id": 14,
+        "img": "Thing.jpg",
+        "name": "Thing",
+        "alterEgo": "Benjamin 'Ben' Grimm",
+        "height": 190,
+        "weight": 200,
+        "years": 35,
+        "gender": "man"
+    },
+    // DC Comics | Catwoman | Selina Kyle | Superhuman strength | 175cm | 61kg | woman | human | Gotham City | black | Not good enough
+    {
+        "id": 15,
+        "img": "Catwoman.jpg",
+        "name": "Catwoman",
+        "alterEgo": "Selina Kyle",
+        "height": 175,
+        "weight": 61,
+        "years": 25,
+        "gender": "woman"
+    },
+    // Marvel Comics | Rogue | Anna Marie | Mastery of magic | 168cm | 54kg | woman | mutant | Avengers Annual #10 (1981) | brown | Not good enough
+    {
+        "id": 16,
+        "img": "Rogue.jpg",
+        "name": "Rogue",
+        "alterEgo": "Anna Marie",
+        "height": 168,
+        "weight": 54,
+        "years": 25,
+        "gender": "woman"
+    },
+    // DC Comics | Atom Girl | Salu Digby | Superhuman strength | 175cm | 61kg | woman | human | Teen Titans/Legion Special #1 (2004) | black | Not good enough
+    {
+        "id": 17,
+        "img": "Atom-Girl.jpg",
+        "name": "Atom Girl",
+        "alterEgo": "Salu Digby",
+        "height": 175,
+        "weight": 61,
+        "years": 25,
+        "gender": "woman"
+    },
+    // Marvel Comics | Black Widow | Natalia Alianovna Romanova | Mastery of magic | 170cm | 59kg | woman | mutant | Tales of Suspense #52 (April, 1964) | red | Not good enough
+    {
+        "id": 18,
+        "img": "Black-Widow.jpg",
+        "name": "Black Widow",
+        "alterEgo": "Natalia Alianovna Romanova",
+        "height": 170,
+        "weight": 59,
+        "years": 25,
+        "gender": "woman"
+    },
+    // Marvel Comics | Deadpool | Wade Wilson | Superhuman strength | 188cm | 95kg | man | mutant | New Mutants #98 (February, 1991) | brown | Neutral
+    {
+        "id": 19,
+        "img": "Deadpool.jpg",
+        "name": "Deadpool",
+        "alterEgo": "Wade Wilson",
+        "height": 188,
+        "weight": 95,
+        "years": 25,
+        "gender": "man"
+    },
+    // Marvel Comics | Phoenix | Jean Grey-Summers | Mastery of magic | 168cm | 52kg | woman | mutant | X-Men #1 (September, 1963) | red | Not good enough
+    {
+        "id": 20,
+        "img": "Phoenix.jpg",
+        "name": "Phoenix",
+        "alterEgo": "Jean Grey-Summers",
+        "height": 168,
+        "weight": 52,
+        "years": 25,
+        "gender": "woman"
+    },
+    // Marvel Comics | Banshee | Sean Cassidy | Mastery of magic | 183cm | 77kg | woman | mutant | X-Men #28 (January, 1967) | blond | Not good enough
+    {
+        "id": 21,
+        "img": "Banshee.jpg",
+        "name": "Banshee",
+        "alterEgo": "Sean Cassidy",
+        "height": 183,
+        "weight": 77,
+        "years": 25,
+        "gender": "woman"
+    },
+    // Marvel Comics | Gamora | Gamora Zen Whoberi Ben Titan | Mastery of magic | 183cm | 77kg | woman | god | Strange Tales #180 (June, 1975) | black | Not good enough
+    {
+        "id": 22,
+        "img": "Gamora.jpg",
+        "name": "Gamora",
+        "alterEgo": "Gamora Zen Whoberi Ben Titan",
+        "height": 183,
+        "weight": 77,
+        "years": 25,
+        "gender": "woman"
+    },
+    // Marvel Comics | Goblin Queen | Madelyne Jennifer Pryor | Mastery of magic | 183cm | 77kg | woman | mutant | Uncanny X-Men #168 | red | Bad
+    {
+        "id": 23,
+        "img": "Goblin-Queen.jpg",
+        "name": "Goblin Queen",
+        "alterEgo": "Madelyne Jennifer Pryor",
+        "height": 183,
+        "weight": 77,
+        "years": 25,
+        "gender": "woman"
+    },
+    // Marvel Comics | Hawkgirl | Kendra Saunders | Mastery of magic | 175cm | 61kg | woman | mutant | Uncanny X-Men #128 | red | Good
+    {
+        "id": 24,
+        "img": "Hawkgirl.jpg",
+        "name": "Hawkgirl",
+        "alterEgo": "Kendra Saunders",
+        "height": 175,
+        "weight": 61,
+        "years": 25,
+        "gender": "woman"
+    },
+    // Marvel Comics | Quicksilver | Pietro Django Maximoff | Superhuman strength | 183cm | 79kg | man | mutant | X-MEN Vol. 1 #4 | white | Not good enough
+    {
+        "id": 25,
+        "img": "Quicksilver.jpg",
+        "name": "Quicksilver",
+        "alterEgo": "Pietro Django Maximoff",
+        "height": 183,
+        "weight": 79,
+        "years": 25,
+        "gender": "man"
+    },
+    // DC Comics | John Constantine | John Constantine | Superhuman strength | 183cm | 89kg | man | human | Swamp Thing Vol 2 #37 (June, 1985) | blond | Not good enough
+    {
+        "id": 26,
+        "img": "John-Constantine.jpg",
+        "name": "John Constantine",
+        "alterEgo": "John Constantine",
+        "height": 183,
+        "weight": 89,
+        "years": 25,
+        "gender": "man"
+    },
+    // Marvel Comics | Shadow King | Amahl Farouk | Superhuman strength | 185cm | 249kg | man | alien | X-Men #117 (1979) | no hair | Not good enough
+    {
+        "id": 27,
+        "img": "Shadow-King.jpg",
+        "name": "Shadow King",
+        "alterEgo": "Amahl Farouk",
+        "height": 185,
+        "weight": 249,
+        "years": 25,
+        "gender": "man"
+    },
+    // Marvel Comics | Siryn | Theresa Rourke Cassidy | Superhuman strength | 168cm | 52kg | woman | mutant | Spider-Woman (first series) #37 | red | Bad
+    {
+        "id": 28,
+        "img": "Siryn.jpg",
+        "name": "Siryn",
+        "alterEgo": "Theresa Rourke Cassidy",
+        "height": 168,
+        "weight": 52,
+        "years": 25,
+        "gender": "woman"
+    },
+    // DC Comics | Red Hood | Jason Todd | Superhuman strength | 183cm | 81kg | man | human | (as Jason Todd) Batman #357 | red | Bad
+    {
+        "id": 29,
+        "img": "Red-Hood.jpg",
+        "name": "Red Hood",
+        "alterEgo": "Jason Todd",
+        "height": 183,
+        "weight": 81,
+        "years": 25,
+        "gender": "man"
+    },
+    // DC Comics | Sportsmaster | Lawrence Crock | Superhuman strength | 180cm | 90kg | man | human | (as Jason Todd) Batman #177 | black | Strange
+    {
+        "id": 30,
+        "img": "Sportsmaster.jpg",
+        "name": "Sportsmaster",
+        "alterEgo": "Lawrence Crock",
+        "height": 180,
+        "weight": 90,
+        "years": 25,
+        "gender": "man"
+    },
+    // DC Comics | General Zod | Dru-Zod | Superhuman strength | 190cm | 100kg | man | alien | Action Comics #845 | black | Bad
+    {
+        "id": 31,
+        "img": "General-Zod.jpg",
+        "name": "General Zod",
+        "alterEgo": "Dru-Zod",
+        "height": 190,
+        "weight": 100,
+        "years": 25,
+        "gender": "man"
     }
-
 ];
 //characters[0].
